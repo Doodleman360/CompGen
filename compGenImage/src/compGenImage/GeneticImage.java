bf6660a849b9b1f32c9ed5b4b12dd4b61f4f438d
Doodleman360 <julislo123@gmail.com>
2016-02-14 15:45:40 -0700

    tweeked values and added more customozation

--- compGenImage/src/compGenImage/GeneticImage.java
+++ compGenImage/src/compGenImage/GeneticImage.java
@@ -22,81 +22,60 @@ import org.uncommons.watchmaker.framework.SelectionStrategy;
 import org.uncommons.watchmaker.framework.operators.EvolutionPipeline;
 import org.uncommons.watchmaker.framework.operators.IntArrayCrossover;
 import org.uncommons.watchmaker.framework.selection.SigmaScaling;
+import org.uncommons.watchmaker.framework.selection.TruncationSelection;
 import org.uncommons.watchmaker.framework.termination.Stagnation;
 
 @SuppressWarnings("serial")
 class GeneticImage extends JComponent {
-    // twekable
-    static int C = 50; // number of spawn per generation
-    static int crossNumber = 2;
-    // nonTwekable
     static Random rand = new Random();
     MersenneTwisterRNG rng = new MersenneTwisterRNG();
-    final static int HEIGHT = 50;
-    final static int WIDTH = 50;
+    final static int DIMENTIONS = 50;
 
-    GeneticImage() {
+    /**
+     *
+     * @param C The number of organisms per generation.
+     * @param mutateProb The probability of mutation.
+     * @param displayIter How often to display generation number and fitness.
+     */
+    GeneticImage(int C, double mutateProb, final int displayIter) {
 
 	int[] ints = new int[16777216];
 	for (int i = 0; i < ints.length; i++) {
 	    ints[i] = i;
 	}
-	CandidateFactory<int[]> factory = new IntArrayFactory(ints, 2500);
+	CandidateFactory<int[]> factory = new IntArrayFactory(ints, (int) Math.pow(DIMENTIONS, 2));
 	LinkedList<EvolutionaryOperator<int[]>> operators = new LinkedList<EvolutionaryOperator<int[]>>();
-	//operators.add(new IntArrayCrossover());
-	operators.add(new IntArrayMutation(ints, new Probability(0.02)));
+	// operators.add(new IntArrayCrossover());
+	operators.add(new IntArrayMutation(ints, new Probability(mutateProb)));
 
 	EvolutionaryOperator<int[]> pipeline = new EvolutionPipeline<int[]>(operators);
 	FitnessEvaluator<int[]> evaluator = new IntArrayEvaluator();
-	SelectionStrategy<Object> strategy = new SigmaScaling();
+	SelectionStrategy<Object> strategy = new TruncationSelection(0.1);
 
 	EvolutionEngine<int[]> engine = new GenerationalEvolutionEngine<int[]>(factory, pipeline, evaluator, strategy,
 		rng);
 
 	engine.addEvolutionObserver(new EvolutionObserver<int[]>() {
 	    public void populationUpdate(PopulationData<? extends int[]> data) {
-		if(data.getGenerationNumber() % 50 == 0) {
-		    System.out.println("Generation " + data.getGenerationNumber() + " with fitness: " + data.getBestCandidateFitness());
+		if (data.getGenerationNumber() % displayIter == 0) {
+		    System.out.println("Generation " + data.getGenerationNumber() + " with fitness: "
+			    + data.getBestCandidateFitness());
 		}
-		
 	    }
 	});
 
-	saveImage(engine.evolve(C, 0, new Stagnation(1000, true)), 0);
+	saveImage(engine.evolve(C, 0, new Stagnation(1000, true)));
 	System.out.println("Finished");
     }
 
-    // Support functions
-
-    static void saveImage(int[] imageData, int name) {
+    static void saveImage(int[] imageData) {
 	try {
 	    BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	    // retrieve image
 	    image.setRGB(0, 0, WIDTH, HEIGHT, imageData, 0, WIDTH);
-	    ImageIO.write(image, "png", new File("art" + name + ".png"));
+	    ImageIO.write(image, "png", new File("art.png"));
 	} catch (IOException e) {
 	    System.out.println("---> CAN'T SAVE FILE <---");
 	}
-    }
-
-    static int[] crossover(int[] parent1, int[] parent2) {
-	int cN = rand.nextInt(crossNumber);
-	int[] child = parent1;
-	for (int i = 0; i < cN; i++) {
-	    int mult = i * child.length / cN;
-	    for (int j = 0; j < child.length / cN; j++) {
-		if (i % 2 == 0) {
-		    child[j + mult] = parent2[j + mult];
-		}
-	    }
-	}
-	return child;
-    }
-
-    static double map(double oldMin, double oldMax, double newMin, double newMax, double oldValue) {
-	double oldRange = (oldMax - oldMin);
-	double newRange = (newMax - newMin);
-	double newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin;
-	return newValue;
     }
 }
--- compGenImage/src/compGenImage/Main.java
+++ compGenImage/src/compGenImage/Main.java
@@ -5,12 +5,15 @@ import org.uncommons.watchmaker.framework.EvolutionEngine;
 @SuppressWarnings("unused")
 
 public class Main {
+    
+    final static int C = 100;
+    final double MUTATEPROB = 0.001;
 
 	public static void main(String[] args) {
 		
 		EvolutionEngine<int[]> engine;
 		
-		GeneticImage genI = new GeneticImage();
+		GeneticImage genI = new GeneticImage(C, 0.001, 200);
 
 	}
 
